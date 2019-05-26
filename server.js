372a7b99ba33a246c66d6d07143b082fb46e028c
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-05-26 16:45:56 -0700

    trying to deploy via Git

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+const http = require('http');
+const port = process.env.PORT || 8080;
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end('<h1>Hello World</h1>');
+});
+server.listen(port, () => {
+	console.log(`Server running at port ` + port);
+});
+
+// // Sets an initial port. We"ll use this later in our listener
+// var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-p.use(express.static(__dirname + "/css"));
+// p.use(express.static(__dirname + "/css"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +33,6 @@ require(path.join(__dirname, '/app/routing/apiroutes.js'))(app);
 require(path.join(__dirname, '/app/routing/htmlRoutes.js'))(app);
 
 
-app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
-});
+// app.listen(port, function() {
+// 	console.log("App listening on PORT: " + port);
+// });

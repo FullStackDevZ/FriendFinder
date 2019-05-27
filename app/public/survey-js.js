417f86d7a415de810edb7360f19b78be7ae86ea4
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-05-26 22:58:22 -0700

    fixed node

--- app/public/survey-js.js
+++ app/public/survey-js.js
@@ -1,62 +1,73 @@
-    var config = {
-      ".chosen-select": {},
-      ".chosen-select-deselect": { allow_single_deselect: true },
-      ".chosen-select-no-single": { disable_search_threshold: 10 },
-      ".chosen-select-no-results": { no_results_text: "Oops, nothing found!" },
-      ".chosen-select-width": { width: "95%" }
-    };
-    for (var selector in config) {
-      $(selector).chosen(config[selector]);
-    }
-    // What occurs when submit is clicked
-    $("#submit").on("click", function() {
-      // Checks user inputs
-      function checkAnswers() {
-        var isValid = true;
-        $(".form-control").each(function() {
-          if ($(this).val() === "") {
-            isValid = false;
-          }
-        });
-        $(".chosen-select").each(function() {
-          if ($(this).val() === "") {
-            isValid = false;
-          }
-        });
-        return isValid;
-      }
-      // If all required fields are filled
-      if (checkAnswers()) {
-        // Create an object for the user"s data
-        var userData = {
-          name: $("#name").val(),
-          photo: $("#photo").val(),
-          scores: [
-            $("#q1").val(),
-            $("#q2").val(),
-            $("#q3").val(),
-            $("#q4").val(),
-            $("#q5").val(),
-            $("#q6").val(),
-            $("#q7").val(),
-            $("#q8").val(),
-            $("#q9").val(),
-            $("#q10").val()
-          ]
-        };
+<script src="https://code.jquery.com/jquery.js"></script>
 
-        var currentURL = window.location.origin;
-        $.post(currentURL + "/api/friends", userData, function(data) {
+var config = {
+  '.chosen-select': {},
+  '.chosen-select-deselect': { allow_single_deselect: true },
+  '.chosen-select-no-single': { disable_search_threshold: 10 },
+  '.chosen-select-no-results': { no_results_text: 'Oops, nothing found!' },
+  '.chosen-select-width': { width: "95%" }
+}
+for (var selector in config) {
+  $(selector).chosen(config[selector]);
+}
+$(".chosen-select").chosen('destroy');
+$(".chosen-select").chosen({ width: "482px" });
 
-            $("#matchName").text(data.name);
-          $("#matchingPic").attr("src", data.photo);
-          // Show the modal with the best match
-          $("#results").modal("toggle");
-        });
+for (var selector in config) {
+  $(selector).chosen(config[selector]);
+}
+// What occurs when submit is clicked
+$("#submit").on("click", function () {
+  // Checks user inputs
+  function checkAnswers() {
+    var isValid = true;
+    $(".form-control").each(function () {
+      if ($(this).val() === "") {
+        isValid = false;
       }
-      else {
-        alert("Please fill out all fields before submitting!");
-      }
-      return false;
     });
+    $(".chosen-select").each(function () {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
+  // If all required fields are filled
+  if (validateForm()) {
+    // Create an object for the user"s data
+    var userData = {
+      name: $("#name").val(),
+      photo: $("#photo").val(),
+      scores: [
+        $("#q1").val(),
+        $("#q2").val(),
+        $("#q3").val(),
+        $("#q4").val(),
+        $("#q5").val(),
+        $("#q6").val(),
+        $("#q7").val(),
+        $("#q8").val(),
+        $("#q9").val(),
+        $("#q10").val()
+      ]
+    };
+    // Grab the URL of the website
+    var currentURL = window.location.origin;
+    // AJAX post the data to the friends API.
+    $.post(currentURL + "/api/friends", userData, function(data) {
+      // Grab the result from the AJAX post so that the best match's name and photo are displayed.
+      $("#matchName").text(data.name);
+      $("#matchImg").attr("src", data.photo);
+      // Show the modal with the best match
+      $("#resultsModal").modal("toggle");
+    });
+  }
+  else {
+    alert("Please fill out all fields before submitting!");
+  }
+  return false;
+});
+
+
 
--- server.js
+++ server.js
@@ -1,38 +1,20 @@
 var express = require("express");
 var path = require("path");
-var bodyParser = require("body-parser");
 
+
+// Setup Express App
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-const http = require('http');
-const port = process.env.PORT || 8080;
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hello World</h1>');
-});
-server.listen(port, () => {
-	console.log(`Server running at port ` + port);
-});
-
-// // Sets an initial port. We"ll use this later in our listener
-// var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// p.use(express.static(__dirname + "/css"));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-require(path.join(__dirname, '/app/routing/apiroutes.js'))(app);
-require(path.join(__dirname, '/app/routing/htmlRoutes.js'))(app);
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes.js')(app);
 
 
-// app.listen(port, function() {
-// 	console.log("App listening on PORT: " + port);
-// });
+
+// Listens to the specified port
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
